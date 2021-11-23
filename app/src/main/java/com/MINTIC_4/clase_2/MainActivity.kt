828813968eb634e35533fa2c6bea172fcd31f9bd
drrozoc <drrozoc@javeriana.edu.co>
2021-11-23 17:43:23 -0500

    Initial commit_1

---
+++ app/src/main/java/com/MINTIC_4/clase_2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.MINTIC_4.clase_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
